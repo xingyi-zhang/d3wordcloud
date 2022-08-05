db57fd7fa83383ad9ab78492009ebeda1805624d
ealexander <ealexander@ealexander22299.local>
2022-08-05 15:36:27 -0500

    Flattened exp1 directory structure so we don't need to refer to things in parent directories. (Should help fix allen issues.)

--- exp1/cloudTest.py
+++ exp1/cloudTest.py
@@ -6,12 +6,9 @@ from Configures import config
 import csv
 import os,sys 
 
-p = os.path.abspath('..')
-sys.path.insert(2,p)
+from flask_util_js import FlaskUtilJs
 
-from flask_util_js.flask_util_js import FlaskUtilJs
-
-app = Flask(__name__,template_folder = '../exp1/templates')
+app = Flask(__name__)
 app.debug = True
 app.config['WEB_ROOT'] = '/'
 # For flask_util.url_for() in JavaScript: https://github.com/dantezhu/flask_util_js
